a85833b4f9d7407db8c58ebab31a2877125a559d
manu310397 <manojb912@gmail.com>
2022-11-20 09:42:34 +0530

    Added initial configuration

---
+++ src/main/java/com/manoj/gateway/SpringMsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.manoj.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMsApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMsApiGatewayApplication.class, args);
+    }
+
+}
